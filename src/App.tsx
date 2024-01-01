87eaeb7e0cd3b7ec776dd146f1b08ba9604082f8
Kourou <gaetan.aka.kourou@hotmail.fr>
2024-01-01 09:12:22 -0500

    create theme and wrap App children with it

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-
-
-import './App.css'
+import { theme } from './theme/theme';
+import './App.css';
+import { ThemeProvider } from '@emotion/react';
 
 function App() {
 
   return (
     <>
-      
+      <ThemeProvider theme={theme}>
+        <div style={{
+          color: theme.colors.main,
+          flexGrow: 1
+          }}>Le theme a bien ete cree</div>
+      </ThemeProvider>
     </>
   )
 }
---
+++ src/theme/theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+  colors: {
+    main: '#1B3D5D',
+  }
+}
