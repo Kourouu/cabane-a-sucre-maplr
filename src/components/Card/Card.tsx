510aae9b71938ab4617ba8ad793b296bdb571f8b
Kourou <gaetan.aka.kourou@hotmail.fr>
2024-01-06 14:03:09 -0500

    use lint on ItemInCart

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -12,59 +12,66 @@ import { Quantity } from './Quantity/Quantity';
 import * as S from './Card.styles';
 
 type CardProps = {
-  product: ProductOverviewType,
-  active?: boolean
-}
+  product: ProductOverviewType;
+  active?: boolean;
+};
 
-export const Card = ({product, active = true}: CardProps) => {
+export const Card = ({ product, active = true }: CardProps) => {
   const { cartItems, setCartItems } = useContext(CartContext);
-  const currentItemInCart = cartItems.find((item) => item.id === product.id)
+  const currentItemInCart = cartItems.find((item) => item.id === product.id);
 
   const productWithQuantity = {
     ...product,
-    qty: currentItemInCart?.qty ?? 0
-  }
+    qty: currentItemInCart?.qty ?? 0,
+  };
 
+  const [quantity, setQuantity] = useState(currentItemInCart?.qty || 1);
 
-  const [quantity, setQuantity] = useState(currentItemInCart?.qty || 1)
-
-  const itemIndexInCartArray = cartItems.findIndex((item) => item.id === product.id)
-
-  const updateItemQty = () => cartItems.map((item) => item.id === product.id ?
-    {
-      ...item,
-      qty: quantity
-    } 
-  : item
+  const itemIndexInCartArray = cartItems.findIndex(
+    (item) => item.id === product.id
   );
-  
 
-  const addNewItemToCart = () => ([
+  const updateItemQty = () =>
+    cartItems.map((item) =>
+      item.id === product.id
+        ? {
+            ...item,
+            qty: quantity,
+          }
+        : item
+    );
+
+  const addNewItemToCart = () => [
     ...cartItems,
     {
       ...product,
-      qty: quantity
-    }
-  ]);
+      qty: quantity,
+    },
+  ];
 
   const updateCart = () => {
-    setCartItems(itemIndexInCartArray > -1 ? updateItemQty() : addNewItemToCart())
-}
+    setCartItems(
+      itemIndexInCartArray > -1 ? updateItemQty() : addNewItemToCart()
+    );
+  };
 
-  const entries = Object.entries(product).filter((entry) => entry[0] !== 'image' && entry[0] !== 'id');
+  const entries = Object.entries(product).filter(
+    (entry) => entry[0] !== 'image' && entry[0] !== 'id'
+  );
 
   return (
     <>
-      <S.ProductCardContainer
-        to={active ? product.id : ''}
-        active={active}
-      >
-        <Image 
+      <S.ProductCardContainer to={active ? product.id : ''} active={active}>
+        <Image
           src={`data:image/png;base64, ${product.image}`}
           alt={`Photo de ${product.name}`}
         />
         <S.List>
-          {entries.map((entry) => (<li key={entry[0]}>{getPropertyLabel(entry[0])} : {entry[1]}</li>))}
+          {entries.map((entry) => (
+            <li key={entry[0]}>
+              {getPropertyLabel(entry[0])} : {entry[1]}
+            </li>
+          ))}
         </S.List>
       </S.ProductCardContainer>
       <Quantity
@@ -74,5 +81,5 @@ export const Card = ({product, active = true}: CardProps) => {
         updateCart={updateCart}
       />
     </>
-    )
-}
+  );
+};
