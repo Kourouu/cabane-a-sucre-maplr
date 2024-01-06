6617b60bab9154074e54574e829d5e217f1acd3f
Kourou <gaetan.aka.kourou@hotmail.fr>
2024-01-06 14:05:53 -0500

    round to 2 decimals

--- src/components/ItemInCart/ItemInCart.tsx
+++ src/components/ItemInCart/ItemInCart.tsx
@@ -1,33 +1,38 @@
-import { useContext } from "react";
+import { useContext } from 'react';
 
-import { CartContext } from "../../contexts";
+import { CartContext } from '../../contexts';
 
-import { CartItemType } from "../../shared/types"
+import { CartItemType } from '../../shared/types';
 
-import { Image } from "../Image/Image";
+import { Image } from '../Image/Image';
 
 import * as S from './ItemInCart.styles';
 
-export const ItemInCart = ({currentItem}: {currentItem:CartItemType}) => {
+export const ItemInCart = ({ currentItem }: { currentItem: CartItemType }) => {
   const { cartItems } = useContext(CartContext);
   console.log(cartItems);
-  console.log(currentItem)
-
+  console.log(currentItem);
 
   return (
-      <S.ItemInCartContainer>
-        <Image
-          initialWidth
-          src={`data:image/png;base64, ${currentItem.image}`}
-          alt={`Photo de ${currentItem.name}`}
-        />
+    <S.ItemInCartContainer>
+      <Image
+        initialWidth
+        src={`data:image/png;base64, ${currentItem.image}`}
+        alt={`Photo de ${currentItem.name}`}
+      />
+      <div>
+        <div>{currentItem.name}</div>
+        <div>Prix unitaire : {currentItem.price}$</div>
+        <div>Quantite : {currentItem.qty}</div>
         <div>
-          <div>{currentItem.name}</div>
-          <div>Stock restant : {parseInt(currentItem.maxQty, 10) - currentItem.qty}</div>
-          <div>Prix unitaire : {currentItem.price}$</div>
-          <div>Prix total avant taxes : {parseInt(currentItem.price, 10) * currentItem.qty}$</div>
+          Prix total avant taxes :{' '}
+          {(parseFloat(currentItem.price) * currentItem.qty).toFixed(2)}$
         </div>
-
-      </S.ItemInCartContainer>
-    )
-}
+        <div>
+          Stock supplementaire disponible :{' '}
+          {parseInt(currentItem.maxQty, 10) - currentItem.qty}
+        </div>
+      </div>
+    </S.ItemInCartContainer>
+  );
+};
