e38c186bf70ed6e190e8cd77395dc5466e12f91b
Kourou <gaetan.aka.kourou@hotmail.fr>
2024-01-06 13:41:20 -0500

    add rule for single quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { ThemeProvider } from '@emotion/react';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { theme } from './theme/theme';
 
-import { CartContext } from "./contexts";
+import { CartContext } from './contexts';
 
 import { Home } from './components/Home/Home';
 import { Menu } from './components/Menu/Menu';
@@ -13,28 +13,26 @@ import { ProductDetails } from './components/ProductDetails/ProductDetails';
 import { Cart } from './components/Cart/Cart';
 
 import * as S from './App.styles';
-import { CartItemType } from "./shared/types";
+import { CartItemType } from './shared/types';
 
 function App() {
-
   const queryClient = new QueryClient();
 
   const [cartItems, setCartItems] = useState<CartItemType[]>([]);
-
 
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <BrowserRouter>
           <S.Container>
-            <CartContext.Provider value={{cartItems, setCartItems}}>
+            <CartContext.Provider value={{ cartItems, setCartItems }}>
               <Menu />
               <S.Content>
                 <Routes>
-                  <Route path='/' element={<Home />} />
-                  <Route path='/products' element={<Products />} />
-                  <Route path='/products/:id' element={<ProductDetails />} />
-                  <Route path='/cart' element={<Cart />} /> 
+                  <Route path="/" element={<Home />} />
+                  <Route path="/products" element={<Products />} />
+                  <Route path="/products/:id" element={<ProductDetails />} />
+                  <Route path="/cart" element={<Cart />} />
                 </Routes>
               </S.Content>
             </CartContext.Provider>
@@ -42,7 +40,7 @@ function App() {
         </BrowserRouter>
       </ThemeProvider>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/contexts.ts
+++ src/contexts.ts
@@ -1,4 +1,4 @@
-import { createContext } from "react";
-import { CartContextType } from "./shared/types";
+import { createContext } from 'react';
+import { CartContextType } from './shared/types';
 
 export const CartContext = createContext<CartContextType>({cartItems: [], setCartItems: () => null});
